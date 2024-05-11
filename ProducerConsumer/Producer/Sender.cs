da601c8c22df351c2dc3bccc5e557b8d9bcdd2aa
Norman Frieman <ezequiel-d.s@hotmail.com>
2024-05-11 07:07:20 -0300

    add ProducerConsumer

---
+++ ProducerConsumer/Producer/Sender.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+//factory.UserName = "guest";
+//factory.Password = "guest";
+//factory.VirtualHost = "/";
+//factory.HostName = "localhost";
+//factory.Port = 5672;
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        var queueName = "BasicTest";
+        channel.QueueDeclare(queueName, false, false, false, null);
+
+        var message = "Hello world";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", queueName, null, body);
+        Console.WriteLine("Sent message: {0}", message);
+        
+        Console.ReadLine();
+    }
+}
